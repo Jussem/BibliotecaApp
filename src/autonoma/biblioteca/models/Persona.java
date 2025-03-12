b53b06d8b065b0ca731941b55c125c05443c8fb7
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-03-12 17:03:00 -0500

    se agrega la clase persona

---
+++ src/autonoma/biblioteca/models/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.biblioteca.models;
+
+/**
+ *
+ * @author juanb
+ */
+public class Persona {
+    ///////////////////////////////////////////////////////////////////////////////////
+    ///Atributos
+    
+    private String nombre;
+    
+}
