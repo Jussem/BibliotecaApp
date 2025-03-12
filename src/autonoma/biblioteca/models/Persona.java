722dcb50ae66cf0af99cfc498b144672775e6932
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-03-12 17:31:24 -0500

    Cambios en la clase persona

--- src/autonoma/biblioteca/models/Persona.java
+++ src/autonoma/biblioteca/models/Persona.java
@@ -13,5 +13,39 @@ public class Persona {
     ///Atributos
     
     private String nombre;
+    private String documentoIdentidad;
+    private String correoElectronico;
+
+    public Persona(String nombre, String documentoIdentidad, String correoElectronico) {
+        this.nombre = nombre;
+        this.documentoIdentidad = documentoIdentidad;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
     
 }
